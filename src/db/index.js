a7370431b4edc009538520721975e67dea612662
ankit sharma <ankit.sharmaa1423@gmail.com>
2024-05-09 13:44:40 +0530

    database coonection

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_name } from "../constant.js";
+
+
+const connectDB = async()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODBB_URI}/${DB_name}`);
+        console.log(`/n database connected DB_host :  ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("database connection failed", error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
